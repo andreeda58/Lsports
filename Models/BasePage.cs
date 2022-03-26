594192e153527289dd81a1b85c57d7b774f47777
andreeda58 <70017796+andreeda58@users.noreply.github.com>
2022-03-27 01:58:16 +0300

    basepage changed

--- Models/BasePage.cs
+++ Models/BasePage.cs
@@ -12,23 +12,6 @@ namespace Models
     public class BasePage
     {
         protected IWebDriver _driver;
-
-        public bool ElementIsPresent(IWebDriver driver, By locator)
-        {
-            try
-            {
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(drv => drv.FindElement(locator));
-                return true;
-            }
-            catch (Exception e)
-            {
-                Logger.Instance.Add(e.Message);
-                throw new Exception(e.Message);
-            }
-        }
-
-        
         
     }
 }
